5cfb117efd694f409082e11956412cac99bc7b5d
helaine <flores.helaine@gmail.com>
2020-09-26 21:16:56 -0500

    Updates

---
+++ precache-manifest.f51ca3d80d46507d99a0154352035fea.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02b445fa9153becf938a44c3db13e42e",
+    "url": "/melp-reviews/index.html"
+  },
+  {
+    "revision": "272d2199c0bfef1d883b",
+    "url": "/melp-reviews/static/css/main.a79dc2be.chunk.css"
+  },
+  {
+    "revision": "77df7aaa8d51c996d51d",
+    "url": "/melp-reviews/static/js/2.96a428cd.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/melp-reviews/static/js/2.96a428cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "272d2199c0bfef1d883b",
+    "url": "/melp-reviews/static/js/main.85ca86ec.chunk.js"
+  },
+  {
+    "revision": "6ed9cd5c924906ab1ff9",
+    "url": "/melp-reviews/static/js/runtime-main.3475b461.js"
+  },
+  {
+    "revision": "5e1314a79a2e25c707a96cfcb4e604c9",
+    "url": "/melp-reviews/static/media/defaultBcg.5e1314a7.jpg"
+  },
+  {
+    "revision": "e9ea73949de71d45ae46047646b95727",
+    "url": "/melp-reviews/static/media/loading-arrow.e9ea7394.gif"
+  },
+  {
+    "revision": "ae3bbefbdb57c400d71f9f469f09369e",
+    "url": "/melp-reviews/static/media/restaurant-2.ae3bbefb.jpg"
+  },
+  {
+    "revision": "ebc1ed9794c6cd400ce0c562f9c806a4",
+    "url": "/melp-reviews/static/media/shareFacebook.ebc1ed97.jpg"
+  }
+]);
